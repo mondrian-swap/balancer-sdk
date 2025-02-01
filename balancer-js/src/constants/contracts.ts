db607497f0fae0a87639ae8f382342463e99b105
The Chimpy Tuts <124721139+chimpytuts@users.noreply.github.com>
2025-02-01 18:03:13 -0300

    Initial commit

---
+++ balancer-js/src/constants/contracts.ts
@@ -0,0 +1,3 @@
+export const balancerVaultMap: { [network: number]: string } = {
+    2741: '0xA06e92c4A9D5Fe540666e1BFf10982d496D5A70D',
+};
---
+++ balancer-js/src/constants/network.ts
@@ -0,0 +1,13 @@
+export enum Network {
+    MAINNET = 1,
+    ROPSTEN = 3,
+    RINKEBY = 4,
+    GÖRLI = 5,
+    KOVAN = 42,
+    POLYGON = 137,
+    ARBITRUM = 42161,
+    AVALANCHE = 43114,
+    FUJI = 43113,
+    VELAS = 106,
+    ABSTRACT = 2741,
+}
